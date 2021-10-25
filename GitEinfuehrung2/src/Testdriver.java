11dfef444e04e58f19a17b8c0fdfacfe9ddc45a8
WeihmayerMarina@dv-schulen.de <WeihmayerMarina@104WKS11.bsz.edu>
2021-10-25 14:09:29 +0200

    commit

--- GitEinfuehrung2/src/Testdriver.java
+++ GitEinfuehrung2/src/Testdriver.java
@@ -5,7 +5,8 @@ public class Testdriver
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hello ");
+		System.out.println("Hello W");
+		
 
 	}//auf projekt, rechtsklick, team, share project
 
